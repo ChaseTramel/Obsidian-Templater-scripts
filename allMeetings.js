3c4b4198bae52bb376713b8c9d51c35832a78c8e
ChaseLittlepaws <littlepaws@hey.com>
2022-07-28 15:10:47 -0400

    Created .js file for use with Obsidian Templater plugin.

---
+++ allMeetings.js
@@ -0,0 +1,39 @@
+//moment.js is already loaded into templater
+//const moment = require('moment');
+//set up function to calculate today and meeting dates
+function allMeetings() {
+    //set up standardized format
+    var format = "dddd, MMMM Do YYYY";
+
+    //tomorrow
+    //calculate tomorrow
+    var today = "Tomorrow is " + moment().add(1, 'd').format(format) + ".  ";
+
+    //weekly meeting
+    //calculate next monday
+    var weeklyMeeting = "Our next weekly meeting is " + moment().endOf('week').day("1").format(format) + ".  ";
+
+    //monthly meeting
+    //calculate first wednesday of the month
+    var monthlyMeeting = "Our next monthly meeting is " + moment().endOf('month').day(3).format(format) + ".  ";
+
+    //quarterly meeting
+    //calculate first saturday of next quarter
+    var quarterlyMeeting = "Our next quarterly meeting is " + moment().endOf('quarter').day(6).format(format) + ".  ";
+
+    //yearly meeting
+    //calculate first sunday of next year
+    var yearlyMeeting = "Our next yearly meeting is " + moment().endOf('year').day(7).format(format) + ".  ";
+
+    //write output to console
+    console.log(today + "\n" + weeklyMeeting + "\n" + monthlyMeeting + "\n" + quarterlyMeeting + "\n" + yearlyMeeting);
+
+    //return output
+    return today + "\n" + weeklyMeeting + "\n" + monthlyMeeting + "\n" + quarterlyMeeting + "\n" + yearlyMeeting;
+}
+
+//run allMeetings function for debugging purposes
+allMeetings();
+
+//export allMeetings for use in Obsidian
+module.exports = allMeetings;
